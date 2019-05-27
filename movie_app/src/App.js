809b3243930affcdf5840841baeed891ccf26ece
jiwookseo <jiwookseo.dev@gmail.com>
2019-05-27 22:20:34 +0900

    Update Axios API call, and setState

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,47 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
+import { thisExpression } from '@babel/types';
+import Axios from 'axios';
 
 class App extends Component {
   state = {
-    greeting: 'hello!',
-    movies: [
-      {
-        title: "Matrix",
-        poster: "https://cdn3.movieweb.com/i/article/xOw6msJtL2YtlnEra7vglma6eLW0yP/1200:100/Matrix-Movie-Code-Secret-Sushi-Recipes.jpg",
-      },
-      {
-        title: 'Oldboy',
-        poster: "https://t1.daumcdn.net/thumb/R1280x0/?fname=http://t1.daumcdn.net/brunch/service/user/2SaY/image/Bqx0WFKTpvrxal3QiYNotjUAN6o.jpg",
-      },
-      {
-        title: 'Endgame',
-        poster: "https://assets1.ignimgs.com/2019/05/18/avengersendgame-thor-2weapons-1280-1558142343257_1280w.jpg",
-      },
-    ],
   }
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        movies: [
-          ...this.state.movies,
-          {
-            title: 'Endgame2',
-            poster: "https://assets1.ignimgs.com/2019/05/18/avengersendgame-thor-2weapons-1280-1558142343257_1280w.jpg",
-          }
-        ]
-      })
-    }, 2000)
+    Axios.get('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+      .then(res => this.setState({movies: res.data.data.movies}))
   }
-
+  _renderMovies = () => {
+    return this.state.movies.map((movie, index) => <Movie title={movie.title} poster={movie.medium_cover_image} key={index} />)
+  }
   render() {
     return (
       <div className="App">
-        {this.state.greeting}
-        {this.state.movies.map((movie, index) =>
-          <Movie title={movie.title} poster={movie.poster} key={index} />
-        )}
+        {this.state.movies ? this._renderMovies() : 'loading'}
       </div>
     )
   }
--- movie_app/src/Movie.js
+++ movie_app/src/Movie.js
@@ -1,31 +1,54 @@
 import React, { Component } from 'react';
 import './Movie.css';
+import PropTypes from 'prop-types';
 
-class Movie extends Component {
-    componentWillMount() {
+// class Movie extends Component { // smart components
+//     static propTypes = {
+//         title: PropTypes.string.isRequired,
+//         poster: PropTypes.string.isRequired,
+//     }
+//     render() {
+//         return (
+//             <div>
+//                 <MoviePoster poster={this.props.poster} />
+//                 <h1>{this.props.title}</h1>
+//             </div>
+//         )
+//     }
+// }
 
+function Movie({title, poster}) { // dumb components, don't have state
+    return (
+        <div>
+            <MoviePoster poster={poster} />
+            <h1>{title}</h1>
+        </div>
+    )
+}
+
+Movie.propTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+}
+
+class MoviePoster extends Component { // smart components
+    static propTypes = {
+        poster: PropTypes.string.isRequired,
     }
-
     render() {
         return (
-            <div>
-                <MoviePoster poster={this.props.poster}/>
-                <h1>{this.props.title}</h1>
-            </div>
+            <img src={this.props.poster} alt="Movie Poster" />
         )
-    }
-
-    componenDidMount() {
-        
     }
 }
 
-class MoviePoster extends Component {
-    render() {
-        return (
-            <img src={this.props.poster} />
-        )
-    }
-}
+// function MoviePoster({ poster }) { // dumb components, don't have state
+//     return (
+//         <img src={poster} alt="Movie Poster" />
+//     )
+// }
+// MoviePoster.propTypes = {
+//     poster: PropTypes.string.isRequired,
+// }
 
 export default Movie
