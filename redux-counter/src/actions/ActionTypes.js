50394956b77bc02ccbcbec7f4d6165dfaf899aad
jiwookseo <jiwookseo.dev@gmail.com>
2019-05-28 17:32:33 +0900

    Update actions

--- redux-counter/src/actions/ActionTypes.js
+++ redux-counter/src/actions/ActionTypes.js
@@ -1,3 +1,6 @@
+export const CREATE = 'CREATE';
+export const REMOVE = 'REMOVE';
+
 export const INCREMENT = 'INCREMENT';
 export const DECREMENT = 'DECREMENT';
 export const SET_COLOR = 'SET_COLOR';
