75fad4b7e73c55a62b7c4fc9ed5cdce7d9eaa67b
jiwookseo <jiwookseo.dev@gmail.com>
2019-05-28 16:57:29 +0900

    Add redux devtools

--- redux-counter/src/index.js
+++ redux-counter/src/index.js
@@ -7,7 +7,8 @@ import { createStore } from 'redux'
 import reducers from './reducers';
 import { Provider } from 'react-redux';
 
-const store = createStore(reducers);
+const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
 
 ReactDOM.render(
     <Provider store={store}>
