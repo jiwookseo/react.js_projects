6cd13b2ed41d8caa1cdceb0cccab70867a7f8d06
jiwookseo <jiwookseo.dev@gmail.com>
2019-05-28 16:16:10 +0900

    Add Provider

--- redux-counter/src/index.js
+++ redux-counter/src/index.js
@@ -5,7 +5,13 @@ import './index.css';
 
 import { createStore } from 'redux'
 import reducers from './reducers';
+import { Provider } from 'react-redux';
 
 const store = createStore(reducers);
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
