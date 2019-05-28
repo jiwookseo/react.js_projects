067a28b1fe148533e7e8f717f2958f5242ffa7ff
jiwookseo <jiwookseo.dev@gmail.com>
2019-05-28 17:32:43 +0900

    Update reducers

--- redux-counter/src/reducers/index.js
+++ redux-counter/src/reducers/index.js
@@ -1,11 +1,70 @@
-import number from './number'
-import color from './color'
+import * as types from '../actions/ActionTypes';
 
-import { combineReducers } from 'redux';
+const initialState = {
+    counters: [
+        {
+            color: 'black',
+            number: 0,
+        }
+    ]
+}
 
-const reducers = combineReducers({
-    numberData: number,
-    colorData: color,
-});
+function counter(state = initialState, action) {
+    const { counters } = sate;
+    switch (action.type) {
+        case types.CREATE:
+            return {
+                counters: [
+                    ...counters,
+                    {
+                        color: action.color,
+                        number: 0
+                    }
+                ]
+            };
+        case types.REMOVE:
+            return {
+                counters: counters.slice(0, counters.length - 1)
+            };
 
-export default reducers;
+        case types.INCREMENT:
+            return {
+                counters: [
+                    ...counters.slice(0, action.index),
+                    {
+                        ...counters[action.index],
+                        number: counters[action.index].number + 1
+                    },
+                    ...counters.slice(action.index + 1, counters.length)
+                ]
+            };
+
+        // action.index 번째 카운터의 number 에 1 을 뺍니다
+        case types.DECREMENT:
+            return {
+                counters: [
+                    ...counters.slice(0, action.index),
+                    {
+                        ...counters[action.index],
+                        number: counters[action.index].number - 1
+                    },
+                    ...counters.slice(action.index + 1, counters.length)
+                ]
+            };
+
+        // action.index 번째 카운터의 색상을 변경합니다
+        case types.SET_COLOR:
+            return {
+                counters: [
+                    ...counters.slice(0, action.index),
+                    {
+                        ...counters[action.index],
+                        color: action.color
+                    },
+                    ...counters.slice(action.index + 1, counters.length)
+                ]
+            };
+        default:
+            return state;
+    }
+}
