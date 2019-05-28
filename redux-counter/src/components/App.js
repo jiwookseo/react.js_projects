2f52a305ad7060ec1795dd6f8bc611ec9550c8d5
jiwookseo <jiwookseo.dev@gmail.com>
2019-05-28 15:54:48 +0900

    Update components and default directory

---
+++ redux-counter/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Counter from './Counter'
+class App extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <Counter></Counter>
+            </div>
+         );
+    }
+}
+ 
+export default App;
