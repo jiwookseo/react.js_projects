13b63bd53bba504eecc3572a348d85d9cb23e82a
jiwookseo <jiwookseo.dev@gmail.com>
2019-05-28 17:32:16 +0900

    Add Button CounterList components

--- redux-counter/src/components/Counter.js
+++ redux-counter/src/components/Counter.js
@@ -2,36 +2,40 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Counter.css';
 
-const Counter = ({ number, color, onIncrement, onDecrement, onSetColor }) => {
+const Counter = ({number, color, index, onIncrement, onDecrement, onSetColor}) => {
     return (
-        <div
-            className="Counter"
-            onClick={onIncrement}
-            onContextMenu={e => {
-                e.preventDefault();
-                onDecrement();
-            }}
-            onDoubleClick={onSetColor}
-            style={{ backgroundColor: color }}>
-            {number}
+        <div 
+            className="Counter" 
+            onClick={() => onIncrement(index)} 
+            onContextMenu={
+                (e) => { 
+                    e.preventDefault(); 
+                    onDecrement(index);
+                }
+            } 
+            onDoubleClick={() => onSetColor(index)}
+            style={{backgroundColor: color}}>
+                {number}
         </div>
     );
 };
 
 Counter.propTypes = {
+    index: PropTypes.number,
     number: PropTypes.number,
     color: PropTypes.string,
     onIncrement: PropTypes.func,
     onDecrement: PropTypes.func,
-    onSetColor: PropTypes.func,
+    onSetColor: PropTypes.func
 };
 
 Counter.defaultProps = {
+    index: 0,
     number: 0,
     color: 'black',
     onIncrement: () => console.warn('onIncrement not defined'),
     onDecrement: () => console.warn('onDecrement not defined'),
-    onSetColor: () => console.warn('onSetColor not defined'),
+    onSetColor: () => console.warn('onSetColor not defined')
 };
 
 export default Counter;
